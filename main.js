2bf32d8644d6d62a85d307af1588a13b6224087c
Inanc <inancakduvann@gmail.com>
2020-08-08 14:02:37 +0300

    close menu warn added in content

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ const targetedArea = document.getElementById("wrapper");
 // Code your functions you will run on swipe!
 function openMenu() {
     document.getElementById("menu").classList.add("menu-active");
+
+    document.getElementById("content").innerHTML = "Swipe up to close menu!";
 }
 
 function closeMenu() {
     document.getElementById("menu").classList.remove("menu-active");
+
+    document.getElementById("content").innerHTML = "Swipe down to open menu!";
 }
 
 // Initialize fingerswipe function with 3 parameters: (direction, target, functions object)
